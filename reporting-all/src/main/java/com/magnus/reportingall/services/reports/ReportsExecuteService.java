3b2d6ae8efdcae602fc235f76d88c5824d6ceeb7
Ernesto Noble <enoble2009@gmail.com>
2017-08-20 19:24:41 -0300

    Fixing merge

--- reporting-all/src/main/java/com/magnus/reportingall/services/reports/ReportsExecuteService.java
+++ reporting-all/src/main/java/com/magnus/reportingall/services/reports/ReportsExecuteService.java
@@ -1,7 +1,6 @@
 package com.magnus.reportingall.services.reports;
 
 import com.magnus.reportingall.domain.reports.Report;
-<<<<<<< HEAD
 import com.magnus.reportingall.exceptions.ServiceException;
 
 public interface ReportsExecuteService {
@@ -15,18 +14,5 @@ public interface ReportsExecuteService {
 	 * @throws ServiceException 
 	 */
 	String executeHTML(Report report) throws ServiceException;
-=======
-
-public interface ReportsExecuteService {
-
-	/**
-	 * 
-	 * Execute report passed as parameter and return HTML as reponse.
-	 * 
-	 * @param report
-	 * @return
-	 */
-	String executeHTML(Report report);
->>>>>>> branch 'master' of https://github.com/enoble2009/reportingall.git
 	
 }
